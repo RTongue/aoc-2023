38501084faa0db4cb5ae0be17a7b426956c7b802
RTongue <r.tongue@gmail.com>
2023-12-06 08:48:43 -0500

    Day 6 WIP

--- day6/index.spec.ts
+++ day6/index.spec.ts
@@ -1,15 +1,17 @@
 import { getInput } from '../utils/getInput'
-import { processFirstPuzzle, processSecondPuzzle } from './processInput'
+import { Race, processFirstPuzzle, processSecondPuzzle } from './processInput'
 
 describe('Day 6', () => {
-  let input = ''
+  let input = `Time:        56     71     79     99
+  Distance:   334   1135   1350   2430`
   const actualInput = async (): Promise<string> => {
     if (input === '') {
       input = await getInput('https://adventofcode.com/2023/day/6/input')
     }
     return input
   }
-  const exampleInput = ``
+  const exampleInput = `Time:      7  15   30
+  Distance:  9  40  200`
 
   describe('first puzzle', () => {
     it('processes the example input', () => {
@@ -17,10 +19,32 @@ describe('Day 6', () => {
       expect(answer).toEqual(0)
     })
 
-    it.skip('processes the first puzzle input', async () => {
+    it('processes the first puzzle input', async () => {
       const input = await actualInput()
       const answer = processFirstPuzzle(input)
       expect(answer).toEqual(0)
+    })
+
+    it('gets if pressing the button for given seconds will beat the record', () => {
+      const race = new Race(7, 9)
+      expect(race.getWillWin(0)).toBe(false)
+      expect(race.getWillWin(1)).toBe(false)
+      expect(race.getWillWin(2)).toBe(true)
+      expect(race.getWillWin(3)).toBe(true)
+      expect(race.getWillWin(4)).toBe(true)
+      expect(race.getWillWin(5)).toBe(true)
+      expect(race.getWillWin(6)).toBe(false)
+      expect(race.getWillWin(7)).toBe(false)
+    })
+
+    it('gets the number of ways to win for race 1', () => {
+      const race = new Race(7, 9)
+      expect(race.numWaysToBeatRecord()).toBe(4)
+    })
+
+    it.only('gets the number of ways to win for race 2', () => {
+      const race = new Race(15, 40)
+      expect(race.numWaysToBeatRecord()).toBe(8)
     })
   })
 
--- day6/processInput.ts
+++ day6/processInput.ts
@@ -1,4 +1,89 @@
+export class Race {
+  time: number = 0
+  distance: number = 0
+
+  constructor(time: number, distance: number) {
+    this.time = time
+    this.distance = distance
+  }
+
+  numWaysToBeatRecord() {
+    const start = Math.floor(this.time / 2)
+    let lowerBound = 0
+    let upperBound = this.time
+
+    let lowerSearchNum = start
+    let lastLowerSearchNum = lowerBound
+    const cachedResults: boolean[] = []
+    let i = 10
+    while (i < 5) {
+      // console.log('lowerSearchNum', lowerSearchNum, 'lastLowerSearchNum', lastLowerSearchNum)
+      const willWin = this.getWillWin(lowerSearchNum )
+      cachedResults[lowerSearchNum] = willWin
+      if (willWin && cachedResults[lowerSearchNum - 1] === undefined) {
+        // continue search
+        lastLowerSearchNum = lowerSearchNum
+        lowerSearchNum = Math.floor(lowerSearchNum / 2)
+      } else if (!willWin && cachedResults[lowerSearchNum + 1] === undefined) {
+        lowerSearchNum = Math.floor((lastLowerSearchNum - lowerSearchNum) / 2) + lowerSearchNum
+      } else if (willWin && cachedResults[lowerSearchNum - 1] === false) {
+        break
+      } else if (!willWin && cachedResults[lowerSearchNum + 1] === true) {
+        lowerSearchNum = lowerSearchNum + 1
+        break
+      }
+      i++
+    }
+
+    let upperSearchNum = start
+    let lastUpperSearchNum = upperBound
+    while (i < 10) {
+      // console.log('upperSearchNum', upperSearchNum, 'lastUpperSearchNum', lastUpperSearchNum)
+      const willWin = this.getWillWin(upperSearchNum)
+      // console.log('willWin', willWin)
+      cachedResults[upperSearchNum] = willWin
+      // console.log('cachedResults', cachedResults)
+      if (willWin && cachedResults[upperSearchNum + 1] === undefined) {
+        // continue search
+        upperSearchNum = Math.floor((lastUpperSearchNum - upperSearchNum) / 2) + upperSearchNum
+      } else if (!willWin && cachedResults[upperSearchNum - 1] === undefined) {
+        lastUpperSearchNum = upperSearchNum
+        upperSearchNum = Math.floor((upperSearchNum - lastUpperSearchNum) / 2) - upperSearchNum
+      } else if (willWin && cachedResults[upperSearchNum + 1] === false) {
+        break
+      } else if (!willWin && cachedResults[upperSearchNum - 1] === true) {
+        upperSearchNum = upperSearchNum - 1
+        break
+      }
+      i++
+    }
+
+    console.log(upperSearchNum, lowerSearchNum)
+    return upperSearchNum - lowerSearchNum + 1
+  }
+
+  getWillWin(milliSecondsPressed: number): boolean {
+    const millimeterPerMilliSecond = milliSecondsPressed
+    return (this.time - milliSecondsPressed) * millimeterPerMilliSecond > this.distance
+  }
+}
+
 export function processFirstPuzzle(input: string): number {
+  const [timesStr, distancesStr] = input.split('\n')
+  const times = timesStr.split('Time:')[1].split(' ')
+    .filter(s => s !== '').map(Number)
+  const distances = distancesStr.split('Distance:')[1].split(' ')
+    .filter(s => s !== '').map(Number)
+  const races: Race[] = []
+
+  for (let i = 0; i < times.length; i++) {
+    const time = times[i]
+    const distance = distances[i]
+    races.push(new Race(time, distance))
+  }
+
+  
+
   return 0
 }
 
