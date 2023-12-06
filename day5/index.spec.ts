298578a2281761352df0f5d095be737e1ab40566
RTongue <r.tongue@gmail.com>
2023-12-05 23:03:15 -0500

    Brute force solution for Day 5

--- day5/index.spec.ts
+++ day5/index.spec.ts
@@ -2,6 +2,13 @@ import { getInput } from '../utils/getInput'
 import { Almanac, processFirstPuzzle, processSecondPuzzle } from './processInput'
 
 describe('Day 2', () => {
+  let input = ''
+  const actualInput = async (): Promise<string> => {
+    if (input === '') {
+      input = await getInput('https://adventofcode.com/2023/day/5/input')
+    }
+    return input
+  }
   const exampleInput = `seeds: 79 14 55 13
 
   seed-to-soil map:
@@ -38,7 +45,7 @@ describe('Day 2', () => {
 
   describe('first puzzle', () => {
     it('gets the list of seeds for the actual input', async () => {
-      const input = await getInput('https://adventofcode.com/2023/day/5/input')
+      const input = await actualInput()
       const almanac = new Almanac(input)
       expect(almanac.seeds).toStrictEqual(new Set([
         41218238, 
@@ -64,25 +71,105 @@ describe('Day 2', () => {
       ]))
     })
 
-    it.only('gets the list of seeds for the example input', async () => {
+    it('gets the list of seeds for the example input', () => {
       const almanac = new Almanac(exampleInput)
       expect(almanac.seeds).toStrictEqual(new Set([
         79, 14, 55, 13
       ]))
     })
 
-    it.skip('processes the first puzzle input', async () => {
-      const input = await getInput('https://adventofcode.com/2023/day/5/input')
+    it('parses the source destination map', () => {
+      const almanac = new Almanac(exampleInput)
+      expect(almanac.conversionMap.seed).toBeTruthy()
+      expect(almanac.conversionMap.seed.convert(79)).toEqual(81)
+      expect(almanac.conversionMap.seed.convert(14)).toEqual(14)
+      expect(almanac.conversionMap.seed.convert(55)).toEqual(57)
+      expect(almanac.conversionMap.seed.convert(13)).toEqual(13)
+    })
+
+    it.only('processes the exmple puzzle input', () => {
+      const answer = processFirstPuzzle(exampleInput)
+      expect(answer).toEqual(35)
+    })
+
+    it('processes the first puzzle input', async () => {
+      const input = await actualInput()
       const answer = processFirstPuzzle(input)
-      expect(answer).toEqual(input)
+      expect(answer).toEqual(251346198)
     })
   })
 
   describe('second puzzle', () => {
+    it('gets the seed ranges', () => {
+      const almanac = new Almanac(exampleInput)
+      expect(almanac.seedRanges.length).toBe(2)
+      expect(almanac.seedRanges[0].start).toBe(79)
+      expect(almanac.seedRanges[0].range).toBe(14)
+      expect(almanac.seedRanges[1].start).toBe(55)
+      expect(almanac.seedRanges[1].range).toBe(13)
+    })
+
+    it('processes the second puzzle example', () => {
+      const answer = processSecondPuzzle(exampleInput)
+      expect(answer).toEqual(46)
+    })
+
+    it('gets the correct seed ranges for the actual input', async () => {
+      const input = await actualInput()
+      const almanac = new Almanac(input)
+      expect(almanac.seedRanges.length).toBe(10)
+      expect(almanac.seedRanges.length).toBe(almanac.seeds.size / 2)
+      expect(almanac.seedRanges[0].start).toBe(41218238)
+      expect(almanac.seedRanges[0].range).toBe(421491713)
+      expect(almanac.seedRanges[1].start).toBe(1255413673)
+      expect(almanac.seedRanges[1].range).toBe(350530906)
+      expect(almanac.seedRanges[2].start).toBe(944138913)
+      expect(almanac.seedRanges[2].range).toBe(251104806)
+
+      expect(almanac.seedRanges[3].start).toBe(481818804)
+      expect(almanac.seedRanges[3].range).toBe(233571979)
+
+      expect(almanac.seedRanges[4].start).toBe(2906248740)
+      expect(almanac.seedRanges[4].range).toBe(266447632)
+
+      expect(almanac.seedRanges[5].start).toBe(3454130719)
+      expect(almanac.seedRanges[5].range).toBe(50644329)
+
+      expect(almanac.seedRanges[6].start).toBe(1920342932)
+      expect(almanac.seedRanges[6].range).toBe(127779721)
+
+      expect(almanac.seedRanges[7].start).toBe(2109326496)
+      expect(almanac.seedRanges[7].range).toBe(538709762)
+
+      expect(almanac.seedRanges[8].start).toBe(3579244700)
+      expect(almanac.seedRanges[8].range).toBe(267233350)
+
+      expect(almanac.seedRanges[9].start).toBe(4173137165)
+      expect(almanac.seedRanges[9].range).toBe(60179884)
+    })
+
+    it('check for overlap in ranges', async () => {
+      const input = await actualInput()
+      const almanac = new Almanac(input)
+      const sortedRanges = almanac.seedRanges.sort((a, b) => {
+        return a.start - b.start
+      })
+      console.log(sortedRanges)
+
+      const endSortedRanges = almanac.seedRanges.sort((a, b) => {
+        return a.end - b.end
+      })
+      console.log(endSortedRanges)
+    })
+
+    it('calculate total seeds', () => {
+      
+    })
+
     it.skip('processes the second puzzle input', async () => {
-      const input = await getInput('https://adventofcode.com/2023/day/5/input')
+      const input = await actualInput()
       const answer = processSecondPuzzle(input)
-      expect(answer).toEqual(input)
+      expect(answer).toEqual(72263011)
     })
   })
 })
--- day5/processInput.ts
+++ day5/processInput.ts
@@ -1,45 +1,165 @@
-export class ConversionMap {
+class SeedRange {
+  start: number
+  end: number
+  range: number
+
+  constructor(start: number, range: number) {
+    this.start = start
+    this.end = start + range - 1
+    this.range = range
+  }
+
+  overlap(seedRange: SeedRange) {
+    return (this.start >= seedRange.start && this.start <= seedRange.end) ||
+      (this.end >= seedRange.start && this.end <= seedRange.end)
+  }
+}
+
+class Range {
+  destStart: number
+  sourceStart: number
+  range: number
+  difference: number
+
+  constructor(
+    destStart: number,
+    sourceStart: number,
+    range: number
+  ) {
+    this.destStart = destStart
+    this.sourceStart = sourceStart
+    this.range = range
+    this.difference = sourceStart - destStart
+  }
+
+  numInRange(num: number): boolean {
+    return num >= this.sourceStart && num <= this.sourceStart + this.range
+  }
+
+  getDest(num: number): number {
+    if (this.numInRange(num)) {
+      return num - this.difference
+    } else {
+      return num
+    }
+  }
+}
+
+export class Conversion {
   source: string
   destination: string
-  sourceRange: number[]
-  destinationRange: number[]
+  rangeList: Range[]
+
+  constructor(source: string, destination: string) {
+    this.source = source
+    this.destination = destination
+    this.rangeList = []
+  }
+
+  convert(num: number): number {
+    for (const range of this.rangeList) {
+      if (range.numInRange(num)) {
+        return range.getDest(num)
+      }
+    }
+    return num
+  }
 }
 
 export class Almanac {
   seeds: Set<number>
-  // Conversion Maps indexed by source, i.e. { seed: { source: 'seed', destination: 'soi' } }
-  conversionMaps: Record<string, ConversionMap>
+  seedRanges: SeedRange[]
+  // Conversions indexed by source, i.e. { seed: { source: 'seed', destination: 'soil', rangeList: [Range] } }
+  conversionMap: Record<string, Conversion>
 
   constructor(almanacString: string) {
     this.parse(almanacString)
   }
 
-  parse(almanacString: string) {
+  parse(almanacString: string): void {
     const lines: string[] = almanacString.split('\n').map(l => l.trim())
     const firstLine = lines.shift()
     if (firstLine === undefined) throw new Error('No seeds in almanac')
     const [, seedNumString] = firstLine.split(':').map(s => s.trim())
-    this.seeds = new Set(seedNumString.split(' ').map(Number))
+    const seedNums = seedNumString.split(' ').map(Number)
+    this.seeds = new Set(seedNums)
+    this.seedRanges = seedNums.reduce((accum, n, i, arr) => {
+      if (i % 2 === 0) {
+        const seedRange = new SeedRange(n, arr[i + 1])
+        accum.push(seedRange)
+      }
+      return accum
+    }, [] as SeedRange[])
+    this.conversionMap = {}
 
     let line = lines.shift()
+    let currentConversionMap
 
-    while (line) {
+    while (line !== undefined) {
       line = line.trim()
       if (line !== '') {
-        if (line.includes('to')) {
-         const [sourceDestStr] = line.split(' ').map(s = s.trim()) 
+        if (line.includes('-to-')) {
+          const [sourceDestStr] = line.split(' ')
+            .map(s => s.trim())
+          const [source, destination] = sourceDestStr.split('-to-')
+            .map(s => s.trim())
+          this.conversionMap[source] = new Conversion(source, destination)
+          currentConversionMap = this.conversionMap[source]
+        } else {
+          let [destNum, sourceNum, range] = line.split(' ')
+            .map(s => Number(s.trim()))
+          currentConversionMap.rangeList
+            .push(new Range(destNum, sourceNum, range))
         }
       }
 
       line = lines.shift()
     }
   }
+
+  convert(num: number, source: string, dest: string): number {
+    const sourceConversion = this.conversionMap[source]
+
+    if (sourceConversion.source === source &&
+      sourceConversion.destination === dest) {
+      return sourceConversion.convert(num)
+    }
+
+    const mappedNum = sourceConversion.convert(num)
+
+    return this.convert(
+      mappedNum, 
+      sourceConversion.destination, 
+      dest
+    )
+  }
 }
 
-export function processFirstPuzzle(input: string): string {
-  return input
+export function processFirstPuzzle(input: string): number {
+  const almanac: Almanac = new Almanac(input)
+  for (const conversion of Object.values(almanac.conversionMap)) {
+    console.log(conversion)
+  }
+  let lowestLocation = Infinity
+
+  for (const seed of almanac.seeds) {
+    const location = almanac.convert(seed, 'seed', 'location')
+    if (location < lowestLocation) lowestLocation = location
+  }
+
+  return lowestLocation
 }
 
-export function processSecondPuzzle(input: string): string {
-  return input
+export function processSecondPuzzle(input: string): number {
+  const almanac: Almanac = new Almanac(input)
+  let lowestLocation = Infinity
+
+  for (const seedRange of almanac.seedRanges) {
+    for (let seed = seedRange.start; seed < seedRange.start + seedRange.range; seed++) {
+      const location = almanac.convert(seed, 'seed', 'location')
+      if (location < lowestLocation) lowestLocation = location
+    }
+  }
+
+  return lowestLocation
 }
