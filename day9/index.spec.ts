f189c6910a5a82e66ecc396262142118bb050d0e
RTongue <r.tongue@gmail.com>
2023-12-13 21:38:15 -0500

    Day 9 solutions

--- day9/index.spec.ts
+++ day9/index.spec.ts
@@ -1,39 +1,42 @@
-import { getInput } from '../utils/getInput'
+import { getFileInput } from '../utils/getInput'
 import { processFirstPuzzle, processSecondPuzzle } from './processInput'
 
-describe('Day 8', () => {
+describe('Day 9', () => {
   let input = ''
   const actualInput = async (): Promise<string> => {
     if (input === '') {
-      input = await getInput('https://adventofcode.com/2023/day/8/input')
+      input = await getFileInput('../day9/input.txt')
     }
     return input
   }
-  const exampleInput = ``
+  const exampleInput = `0 3 6 9 12 15
+  1 3 6 10 15 21
+  10 13 16 21 30 45`
 
   describe('first puzzle', () => {
     it('processes the example input', () => {
       const answer = processFirstPuzzle(exampleInput)
-      expect(answer).toEqual(0)
+      expect(answer).toEqual(114)
     })
 
-    it.skip('processes the first puzzle input', async () => {
+    it('processes the first puzzle input', async () => {
       const input = await actualInput()
+      // console.log(input)
       const answer = processFirstPuzzle(input)
-      expect(answer).toEqual(0)
+      expect(answer).toEqual(1861775706)
     })
   })
 
   describe('second puzzle', () => {
-    it.skip('processes the example input', () => {
+    it('processes the example input', () => {
       const answer = processSecondPuzzle(exampleInput)
-      expect(answer).toEqual(0)
+      expect(answer).toEqual(2)
     })
 
-    it.skip('processes the second puzzle input', async () => {
+    it('processes the second puzzle input', async () => {
       const input = await actualInput()
       const answer = processSecondPuzzle(input)
-      expect(answer).toEqual(0)
+      expect(answer).toEqual(1082)
     })
   })
 })
